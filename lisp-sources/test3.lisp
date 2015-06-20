97c8ccb9f24c49d3ca9b8b4e6de0c6714020a14e
flyingleafe <flyingleafe@gmail.com>
2015-06-20 19:55:33 +0000

    let and local variables

--- lisp-sources/test3.lisp
+++ lisp-sources/test3.lisp
@@ -1,2 +1,2 @@
-(define foo (lambda () 
+(define foo (lambda (a b c) 
               (if (% 4 2) (+ 10 5) (- 10 5))))
---
+++ lisp-sources/test4.lisp
@@ -0,0 +1,4 @@
+(define foo (lambda (a b)
+              (let ((x (+ a 3))
+                    (y (- b 2)))
+                (* x y))))
