525eb19c2d5a75e9590ba5fabbb6a02556dd58c4
volhovM <volhovm.cs@gmail.com>
2015-06-23 13:34:31 +0300

    added operator n-arity

--- src/Builtins.hs
+++ src/Builtins.hs
@@ -19,7 +19,7 @@ import Data.List
   `argn` field is the number of arguments
   `body' determines how function body behaves around its args' bodies.
 --}
-data Builtin = Inline { name :: String, argn :: Int,
+data Builtin = Inline { name :: String, argn :: [Int],
                         body :: [[CodeBlock]] → [CodeBlock] }
 
 -- True if name is reserved for builtin
@@ -28,28 +28,29 @@ builtinName nm = any (\b → name b ≡ nm) builtins
 
 -- Finds builtin with certain name and args number
 getBuiltin :: String → Int → Maybe Builtin
-getBuiltin nm n = find (\b → name b ≡ nm ∧ argn b ≡ n) builtins
+getBuiltin nm n = find (\b → name b ≡ nm ∧ n ∈ argn b) builtins
+
+from :: Int → [Int]
+from n = iterate (+1) n
 
 builtins :: [Builtin]
-builtins = [ Inline "not" 1 not'
-           , Inline "~"   1 btw_not
-           , Inline "neg" 1 neg
-           , Inline "and" 2 and'
-           , Inline "&"   2 and'
-           , Inline "or"  2 or'
-           , Inline "|"   2 or'
-           , Inline "+"   1 nop
-           , Inline "-"   1 neg
-           , Inline "+"   2 plus
-           , Inline "-"   2 minus
-           , Inline "*"   2 mul
-           , Inline "/"   2 div'
-           , Inline "%"   2 mod'
-           , Inline "="   2 equal
-           , Inline "<"   2 less
-           , Inline "<="  2 leq
-           , Inline ">"   2 greater
-           , Inline ">="  2 geq
+builtins = [ Inline "not" [1]      not'      -- this one is lognot, casts to bool
+           , Inline "~"   [1]      btw_not   -- this is bitwise not
+           , Inline "neg" [1]      neg       -- this is int sign negation
+           , Inline "and" (from 1) and'
+           , Inline "or"  (from 1) or'
+
+           , Inline "+"   (from 1) plus
+           , Inline "-"   (from 1) minus
+           , Inline "*"   (from 1) mul
+           , Inline "/"   (from 1) div'
+           , Inline "%"   [2]      mod'
+
+           , Inline "="   (from 1) equal
+           , Inline "<"   [2]      less
+           , Inline "<="  [2]      leq
+           , Inline ">"   [2]      greater
+           , Inline ">="  [2]      geq
            ]
 
 toBool, not', btw_not, neg, and', or', equal, plus, minus,
@@ -68,52 +69,66 @@ btw_not [a] = a ⊕ [CodeBlob [Not "rax"]]
 
 neg [a] = a ⊕ [CodeBlob [Neg "rax"]]
 
+and' [a]    = a
 and' [a, b] = a ⊕
               [CodeBlob [Push "rax"]] ⊕
               b ⊕
               [CodeBlob [Pop "rdx",
                          And "rax" "rdx"]]
+and' (x:xs) = and' $ x:[and' xs]
 
+or' [a]    = a
 or' [a, b] = a ⊕
              [CodeBlob [Push "rax"]] ⊕
              b ⊕
              [CodeBlob [Pop "rdx",
                         Or "rax" "rdx"]]
+or' (x:xs) = or' $ x:[or' xs]
 
-nop [a] = a ⊕ [CodeBlob []]
+nop [a] = a
 
-equal [a, b] = not' [a ⊕
-                     [CodeBlob [Push "rax"]] ⊕
-                     b ⊕
-                     [CodeBlob [Pop "rdx",
-                                Xor "rax" "rdx"]]]
-
+plus [a]    = a
 plus [a, b] = a ⊕
               [CodeBlob [Push "rax"]] ⊕
               b ⊕
               [CodeBlob [Pop "rdx",
                          Add "rax" "rdx"]]
+plus (x:xs) = plus $ x:[plus xs]
 
+minus [a]    = neg [a]
 minus [a, b] = b ⊕
                [CodeBlob [Push "rax"]] ⊕
                a ⊕
                [CodeBlob [Pop "rdx",
                           Sub "rax" "rdx"]]
+minus (x:xs) = minus $ x:[plus xs]
 
+mul [a]    = a
 mul [a, b] = a ⊕
              [CodeBlob [Push "rax"]] ⊕
              b ⊕
              [CodeBlob [Pop "rdx",
                         Mul "rdx"]]
+mul (x:xs) = mul $ x:[mul xs]
 
+div' [a]    = a
 div' [a, b] = b ⊕
              [CodeBlob [Push "rax"]] ⊕
              a ⊕
              [CodeBlob [Pop "rcx",
                         Xor "rdx" "rdx",
                         Div "rcx"]]
+div' (x:xs) = div' $ x:[mul xs]
 
 mod' [a, b] = div' [a, b] ⊕ [CodeBlob [Mov "rax" "rdx"]]
+
+equal [a]    = a
+equal [a, b] = not' [a ⊕
+                     [CodeBlob [Push "rax"]] ⊕
+                     b ⊕
+                     [CodeBlob [Pop "rdx",
+                                Xor "rax" "rdx"]]]
+equal (x:xs) = equal $ x:[equal xs]
 
 less [a, b] = b ⊕
               [CodeBlob [Push "rax"]] ⊕
