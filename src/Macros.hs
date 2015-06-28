7dd7a2642f80ddf1974071df5b4bb078472ac92b
flyingleafe <flyingleafe@gmail.com>
2015-06-28 00:34:30 +0000

    car-cdr multimacroses

--- src/Macros.hs
+++ src/Macros.hs
@@ -18,7 +18,22 @@ macros :: [Macro]
 macros = [ Macro "and" (from 0) and'
          , Macro "or" (from 0) or'
          , Macro "progn" (from 0) Progn
-         ]
+         ] ++ carCdrShortens
+
+carCdrShortens :: [Macro]
+carCdrShortens = map ccShorten allInfixes
+    where ccShorten inf = Macro ("c" ++ inf ++ "r") [1] $ ccFoo inf
+          ccFoo "" [a] = a
+          ccFoo ('a':ss) [a] = Funcall "car" $ [ccFoo ss [a]]
+          ccFoo ('d':ss) [a] = Funcall "cdr" $ [ccFoo ss [a]]
+
+allInfs :: Int → [String]
+allInfs 0 = [""]
+allInfs n = map ('a':) pr' ++ map ('d':) pr'
+    where pr' = allInfs (n - 1)
+
+allInfixes :: [String]
+allInfixes = concatMap allInfs [2..4]
 
 and', or' :: [AExp] → AExp
 and' ss = foldl wrapIf (Const 1) ss
